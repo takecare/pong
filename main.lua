f68224270dd703c232cb5d070b4263299f140d2f
Rui Teixeira <rui@vazteixeira.org>
2019-03-19 12:11:35 +0000

    pong 2

--- main.lua
+++ main.lua
@@ -4,6 +4,10 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+PADDLE_WIDTH = 5
+PADDLE_HEIGHT = 20
+BALL_SIZE = 5
+
 DEFAULT_FONT_HEIGHT = 6
 
 push = require 'push'
@@ -23,6 +27,7 @@ function love.load()
         vsync = true
     })
 
+    -- 
     love.graphics.setDefaultFilter('nearest', 'nearest')
 end
 
@@ -32,11 +37,23 @@ end
 
 -- Called after update by LÖVE2D, used to draw anything to the screen, updated or otherwise.
 function love.draw()
+    -- love.graphics.clear(0.1, 0, 0, 0)
+
     love.graphics.printf('Hello Pong!', 10, 10, WINDOW_WIDTH, 'left') -- can take up whole width
     
     push:apply('start')
-    -- love.graphics.rectangle('fill', 10, 10, 100, 100)
-    love.graphics.printf('Hello Pong!', 10, 10, VIRTUAL_WIDTH, 'left')
+    -- love.graphics.setColor(0, 0, 1, 0)
+    
+    -- left paddle
+    love.graphics.rectangle('fill', PADDLE_WIDTH * 2, VIRTUAL_HEIGHT / 2 - (PADDLE_HEIGHT / 2), 5, 20)
+
+    -- right paddle
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - PADDLE_WIDTH * 3, VIRTUAL_HEIGHT / 2 - (PADDLE_HEIGHT / 2), 5, 20)
+
+    -- ball
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH/2 - (BALL_SIZE/2), VIRTUAL_HEIGHT/2 - (BALL_SIZE/2), BALL_SIZE, BALL_SIZE)
+
+    love.graphics.printf('p o n g', 0, 5, VIRTUAL_WIDTH, 'center')
     push:apply('end')
 end
 
