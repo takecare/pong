1a20d926a79d50993c480c8ef0557dafd65e6442
takecare <rui@vazteixeira.org>
2019-03-28 21:54:33 +0000

    remove commented-out code

--- Paddle.lua
+++ Paddle.lua
@@ -10,10 +10,9 @@ function Paddle:init(x, y, width, height, upKey, downKey, screenWidth, screenHei
    self.downKey = downKey
    self.screenWidth = screenWidth
    self.screenHeight = screenHeight
+
    self.speed = 200
    self.dy = 0
-
-   self.de = 0
 
    -- full length of the border = screenWidth * 2 + screenHeight * 2
    -- we want this to be the amount the top of the paddle has moved across the full length of the border
@@ -27,7 +26,7 @@ end
 
 function Paddle:update(dt)
     self.y = self.y + self.dy * dt
-    self.edge = self.y -- - self.screenHeight/2 + self.height/2
+    self.edge = self.y
 end
 
 function Paddle:render()
@@ -37,42 +36,34 @@ function Paddle:render()
 
     self._msg = 'delta=' .. math.ceil(delta)
 
-    -- if (delta >= self.height) then -- draw on top-most edge
-        -- love.graphics.rectangle('fill', x + delta - self.height + self.width, 0, self.height, self.width)
     if (delta > 0 and delta < self.height - self.width) then
         love.graphics.rectangle('fill', x + self.width, 0, delta, self.width)
         love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
         self._msg = '-> delta=' .. math.ceil(delta)
     elseif (delta >= self.height - self.width) then
         love.graphics.rectangle('fill', x + delta - self.height + self.width, 0, self.height, self.width)
-        -- love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
     else
         love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
     end
-
-    -- love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
 end
 
 function Paddle:handleInput()
-    if love.keyboard.isDown(self.upKey) then--and (self.y >= 0) then
+    if love.keyboard.isDown(self.upKey) then
         self:_moveUpwards()
     elseif love.keyboard.isDown(self.downKey) and (self.y + self.height < self.screenHeight) then
         self:_moveDownwards()
     else 
         self.dy = 0
-        self.de = 0
+        
     end
 end
 
 function Paddle:_moveUpwards()
-    self.de = -self.speed
     self.dy = -self.speed
 end
 
 function Paddle:_moveDownwards()
-    self.de = self.speed
     self.dy = self.speed
-    
 end
 
 function Paddle:debug()
